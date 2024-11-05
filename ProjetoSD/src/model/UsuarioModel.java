70e976645856dbfa8c838e795e32e60a2b0c5991
GOA03 <91618139+GOA03@users.noreply.github.com>
2024-11-05 15:54:54 -0300

    Add files via upload

--- ProjetoSD/src/model/UsuarioModel.java
+++ ProjetoSD/src/model/UsuarioModel.java
@@ -1,32 +1,31 @@
 package model;
 
 public class UsuarioModel {
-    private int id;
-    private int ra;
-    private String nome;
-    private String senha;
-    private String token;
-    private boolean avisosEsportes;
-    private boolean avisosClima;
-    private boolean avisosTecnologia;
-    private boolean avisosSaude;
-    private boolean avisosEconomia;
-
-    // Construtor
-    public void Usuario(int id, int ra, String nome, String senha, String token, 
-                   boolean avisosEsportes, boolean avisosClima, boolean avisosTecnologia, 
-                   boolean avisosSaude, boolean avisosEconomia) {
-        this.id = id;
-        this.ra = ra;
-        this.nome = nome;
-        this.senha = senha;
-        this.token = token;
-        this.avisosEsportes = avisosEsportes;
-        this.avisosClima = avisosClima;
-        this.avisosTecnologia = avisosTecnologia;
-        this.avisosSaude = avisosSaude;
-        this.avisosEconomia = avisosEconomia;
-    }
+	private int id;
+	private int ra;
+	private String nome;
+	private String senha;
+	private String token;
+	private boolean avisosEsportes;
+	private boolean avisosClima;
+	private boolean avisosTecnologia;
+	private boolean avisosSaude;
+	private boolean avisosEconomia;
+ 
+	// Construtor
+	public void Usuario(int id, int ra, String nome, String senha, String token, boolean avisosEsportes,
+			boolean avisosClima, boolean avisosTecnologia, boolean avisosSaude, boolean avisosEconomia) {
+		this.id = id;
+		this.ra = ra;
+		this.nome = nome;
+		this.senha = senha;
+		this.token = token;
+		this.avisosEsportes = avisosEsportes;
+		this.avisosClima = avisosClima;
+		this.avisosTecnologia = avisosTecnologia;
+		this.avisosSaude = avisosSaude;
+		this.avisosEconomia = avisosEconomia;
+	}
 
     // Getters e Setters
     public int getId() { return id; }
